1bd5d8b58c2f643c6e55fd34a1833308f4eef10b
Lyubomira Lyubomirova <lyubomira.lyubomirova@fadata.eu>
2020-05-17 17:27:38 +0300

    added startGame functionality, updateing dashboard and player car on screen

--- script.js
+++ script.js
@@ -3,9 +3,16 @@ const scoreDash = document.querySelector(".score-dash");
 const lifeDash = document.querySelector(".life-dash");
 const container = document.getElementById("container");
 const btnStart = document.querySelector(".btn-start");
-let animationGame = requestAnimationFrame(playGame);
 
+btnStart.addEventListener("click", startGame);
+document.addEventListener("keydown", pressKeyOn);
+document.addEventListener("keyup", pressKeyOff);
+
+//Game Variables
+let animationGame;
 let gamePlay = false;
+let player;
+
 let keys = {
   ArrowUp: false,
   ArrowDown: false,
@@ -13,9 +20,39 @@ let keys = {
   ArrowRight: false,
 };
 
+function startGame() {
+  console.log(gamePlay);
+  btnStart.style.display = "none";
+  var div = document.createElement("div");
+  div.setAttribute("class", "player-car");
+  div.x = 250;
+  div.y = 500;
+
+  container.appendChild(div);
+  gamePlay = true;
+  animationGame = requestAnimationFrame(playGame);
+  player = {
+    speed: 1,
+    lives: 3,
+    gameScore: 0,
+    carstoPass: 10,
+  };
+}
+
+function pressKeyOn() {}
+
+function pressKeyOff() {}
+
+function updateDash() {
+  console.log(player);
+  scoreDash.innerHTML = player.gameScore;
+  lifeDash.innerHTML = player.lives;
+  speedDash.innerHTML = player.speed;
+}
+
 function playGame() {
   if (gamePlay) {
-    console.log("Game in play");
+    updateDash();
   }
   animationGame = requestAnimationFrame(playGame);
 }
